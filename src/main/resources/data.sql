7c18940b5fbc9a8e44079f6b5964a20ebe32473a
mockingbird <nguyenthanhtrungltv@gmail.com>
2021-06-18 01:10:23 +0700

    deploy edit java ver

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,5 @@ insert into app_user values
 (1,'2017-11-11','abc1@gmail.com','đẹp trai','male',null,null,null,'abc1@gmail.com'),
 (2,'2017-11-11','abc2@gmail.com','đẹp trai','male',null,null,null,'abc2@gmail.com'),
 (3,'2017-11-11','abc3@gmail.com','đẹp trai','male',null,null,null,'abc3@gmail.com'),
-(4,'2017-11-11','abc4@gmail.com','đẹp trai','male',null,null,null,'abc4@gmail.com')
 
 
