728e93d018e4d3b34297e3c35a8a55a00c342010
nguyentrung <trung.nguyen@incepit.com>
2023-02-09 11:08:28 +0700

    docker compose

--- src/test/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/com/bezkoder/springjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bezkoder.springjwt;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class SpringBootSecurityJwtApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.bezkoder.springjwt;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class SpringBootSecurityJwtApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
